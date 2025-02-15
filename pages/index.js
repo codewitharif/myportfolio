e93d5fd072ecc2419deba528c5204eb93d0f2ba5
Md Arif <aatif149209@gmail.com>
2025-02-16 00:09:01 +0530

    changed footer commit

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ import userData from "@constants/data";
 export default function Home({ repositories }) {
   return (
     <ContainerBlock
-      title="Manu Arora - Developer, Writer, Creator"
-      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+      title="Md Arif - Developer, Desiner, Programmer"
+      description="This is my portfolio representing my work."
     >
       <Hero />
       <FavouriteProjects />
