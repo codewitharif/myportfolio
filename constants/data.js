4451f0e2f9222a1baf1d8723bcbb933b6070c6bf
Md Arif <aatif149209@gmail.com>
2025-02-15 19:09:03 +0530

    first commit

---
+++ constants/data.js
@@ -0,0 +1,105 @@
+const userData = {
+  githubUsername: "manuarora700",
+  name: "Md Arif",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "mdarif1522003@gmail.com",
+  phone: "+91 7772884698",
+  address: "Bokaro, Jharkhand, India.",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Node.js, Express,  MongoDB, MySQL, Bootstrap and Tailwind.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Software Developer Intern",
+      company: "Biznweb Technologies",
+      year: "2024",
+      companyLink: "https://biznweb.com/",
+      desc: "Contributed to a hotel booking application - Panna - which is an AI powered candidate interviewing product.",
+    },
+    {
+      title: "Founder and Developer",
+      company: "TailwindMasterKit",
+      year: "2021",
+      companyLink: "https://tailwindmasterkit.com",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+
+    {
+      title: "Engineering",
+      company: "Millennim Institute Of Technology & Science, Bhopal.",
+      year: "2025",
+      companyLink: "https://www.millenniumbhopal.com/",
+      desc: "Major in Computer Science and Engineering with a CGPA of 7.2. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "Mithila Academy Public School, Bokaro.",
+      year: "2021",
+      companyLink: "http://maps.bokaro.org.in/",
+      desc: "Subjects being PCM - I barely survived with 72.0% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1ujrxzUV6Bzha4HoESV9DoCxhc5QV1kUm/view?usp=drive_link",
+  socialLinks: {
+    instagram: "https://www.instagram.com/sayarifff",
+    twitter: "https://twitter.com/mdarif1522003",
+    linkedin: "https://linkedin.com/in/mdarif1522003",
+    github: "https://github.com/codewitharif",
+    facebook: "https://facebook.com/",
+    peerlist: "https://peerlist.io/manuarora",
+  },
+};
+
+export default userData;
